55d76cfe762a2249978e12558725390474697c7c
Farooq35 <Farooqawan35@gmail.com>
2017-09-06 00:47:53 +0500

    Starting

---
+++ Travel/SignUp.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+<title></title>
+<link href="css/owl.carousel.css" rel='stylesheet' type='text/css'/>
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css'/>
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all"/>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+ //function hideURLbar(){ window.scrollTo(0,1); } 
+ 
+ 
+ </script>
+<!--js--> 
+<script src="js/jquery.min.js"></script>
+
+<!--/js-->
+<!--animated-css-->
+<link href="css/animate.css" rel="stylesheet" type="text/css" media="all">
+<script src="js/wow.min.js"></script>
+<script>
+ new WOW().init();
+</script>
+<!--/animated-css-->
+</head>
+<body>
+<!--header-->
+<!--sticky-->
+<?php include('top.php'); ?>
+
+
+
+
+</body>
+</html>
